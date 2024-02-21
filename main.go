4d90b4251751b551cbb41c9ad4e854eb8c665ccb
harshvirani7 <160610133+harshvirani7@users.noreply.github.com>
2024-02-21 09:59:06 +0530

    added api format and more info

--- main.go
+++ main.go
@@ -21,3 +21,18 @@ func main() {
 		fmt.Println("Failed to start server:", err)
 	}
 }
+
+// use gin
+// get data from pulsar and modify and store in redis
+
+// query param - event type - return total event count
+// GET - /countByEventType/{eventType}
+// for each esn - count of total no of events - qp - event type
+// GET - /totalEventsByESN/{eventType} // total events of that type for each esn
+// count of event by esn id
+// GET /totalEvents/{ESNID} -> int
+// each event count for the esn
+// GET /esnSummary/{ESNID}
+
+// format data from pulsar and store to redis
+// APIs will fetch data from redis
